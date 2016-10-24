3aee901a82f7d2f02037c3ccec089105fb1badf6
Rikki Tooley <rikki@inflatablefriends.co.uk>
2016-10-24 08:28:10 +0100

    First commit - set up brunch - set up typescript and typings - gitignore - canvas, layer system skeleton, render loop

---
+++ brunch-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+   npm: {
+     globals: {
+      $: 'jquery',
+      jQuery: 'jquery'
+  }},
+  // See http://brunch.io for documentation.
+  files: {
+    javascripts: {
+      joinTo: {
+        'js/app.js': /^app/,
+        'js/vendor.js': /(^node_modules|^vendor)/ 
+      },
+      order: {
+        before: /.*jquery.*/
+      }
+    },
+    stylesheets: { joinTo: 'app.css' },
+    templates: { joinTo: 'app.js' }
+  },
+  plugins: {
+    sass: {
+      options: {
+        includePaths: [
+          'node_modules/bootstrap-sass/assets/stylesheets/'
+        ]
+      }
+    }
+  }
+}
